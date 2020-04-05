0e2b7dabad8cf658c77ca2d560f442d7a699c2ab
Superaph <61982970+Superaph@users.noreply.github.com>
2020-04-05 11:55:19 +0200

    Update Guerre Nucléaire.py
    J'ai mis en commentaire la partie de ton code qui, je pense, peut-être remplacée.
    A la fin j'ai mis un exemple de ce que fait la liste pour l'OTAN et la Corée du Nord. J'en parle dans le commentaire au niveau de ce que j'ai ajouté.

--- Guerre Nucléaire.py
+++ Guerre Nucléaire.py
@@ -19,21 +19,30 @@ def guerre_mondiale(pays_attaquant, pays_attaque):
         print('Le pays attaquant est : ' + pays_attaquant + ', il a ' + str(nb_bombes_attaquant) + ' bombes')
         print('Le pays attaqué est : ' + pays_attaque + ', il a ' + str(nb_bombes_attaque) + ' bombes')
 
-        alliance = ''
-        nb_bombes = 0
-        pays_test = set()
+#        alliance = ''
+#        nb_bombes = 0
+#        pays_test = set()
         
-        while alliance != alliance_attaque or nb_bombes == 0:
-            riposte = random.randint(0,len(pays)-1)
-            pays_test.add(riposte)
-            
-            attaquant = noms_pays[riposte]
-            nb_bombes, alliance = pays[attaquant]
+        #Liste_allies: list[int]
+        Liste_allies = [p for p,(_,alliance) in pays.items() if alliance == alliance_attaque ] 
+        #Cette liste pose problème pour la Corée du Nord et Israël car ils appartiennent tout les deux à "NONE" donc sont considérés comme alliés 
+        #On peut faire un cas particulier pour ces deux pays ou alors mettre des str distincts au niveau de leur "alliance" dans le tuple
+        riposte = random.randint(0,len(Liste_allies)-1)
+        nb_bombes,_ = pays[Liste_allies[riposte]]
+        if nb_bombes != 0: 
+            pays[Liste_allies[riposte]] = (nb_bombes - 1, alliance_attaque)        
+        
+#        while alliance != alliance_attaque or nb_bombes == 0:
+#            riposte = random.randint(0,len(pays)-1)
+#            pays_test.add(riposte)
+#            
+#            attaquant = noms_pays[riposte]
+#            nb_bombes, alliance = pays[attaquant]
+#
+#            if len(pays_test) == len(pays):
+#                attaque = False
 
-            if len(pays_test) == len(pays):
-                attaque = False
-
-        pays[attaquant] = (nb_bombes - 1, alliance)
+#        pays[attaquant] = (nb_bombes - 1, alliance)
 
         pays_attaque = pays_attaquant
         pays_attaquant = attaquant
@@ -41,3 +50,17 @@ def guerre_mondiale(pays_attaquant, pays_attaque):
     print('Le dernier pays attaqué est la/le-s ' + pays_attaque + ' par la/le-s ' + pays_attaquant)
     
     return pays[pays_attaquant]
+
+
+
+
+pays = {'Belgique' : (0, 'OTAN'), 'Canada' : (0, 'OTAN'), 'Danemark' : (0, 'OTAN'), 'Etats-Unis' : (6450, 'OTAN'), 'France' : (300, 'OTAN'), 'Islande' : (0, 'OTAN'), 'Italie' : (0, 'OTAN'), 'Luxembourg' : (0, 'OTAN'), 'Norvège' : (0, 'OTAN'), 'Pays-Bas' : (0, 'OTAN'), 'Portugal' : (0, 'OTAN'), 'Royaume-Uni' : (215, 'OTAN'), 'Grèce' : (0, 'OTAN'), 'Turquie' : (0, 'OTAN'), 'Allemagne' : (0, 'OTAN'), 'Espagne' : (0, 'OTAN'), 'République-Tchèque' : (0, 'OTAN'), 'Pologne' : (0, 'OTAN'), 'Hongrie' : (0, 'OTAN'), 'Bulgarie' : (0, 'OTAN'), 'Estonie' : (0, 'OTAN'), 'Lettonie' : (0, 'OTAN'), 'Lituanie' : (0, 'OTAN'), 'Roumanie' : (0, 'OTAN'), 'Slovaquie' : (0, 'OTAN'), 'Slovénie' : (0, 'OTAN'), 'Albanie' : (0, 'OTAN'), 'Croatie' : (0, 'OTAN'), 'Monténégro' : (0, 'OTAN'), 'Chine' : (270, 'OCS'), 'Russie' : (6600, 'OCS'), 'Kazakhstan' : (0, 'OCS'), 'Kirghizistan' : (0, 'OCS'), 'Tadjikistan' : (0, 'OCS'), 'Ouzbékistan' : (0, 'OCS'), 'Inde' : (130, 'OCS'), 'Pakistan' : (140, 'OCS'), 'Corée du Nord' : (15, 'NONE'), 'Israël' : (80, 'NONE')}
+
+#Un exemple de la liste que j'ai créé sur l'OTAN
+OTAN = [p for p,(_,alliance) in pays.items() if alliance == 'OTAN' ] 
+
+print(OTAN)
+#En bas c'est le problème dont je parle dans le commentaire
+NN = [p for p,(_,alliance) in pays.items() if alliance == 'NONE' ] 
+
+print(NN)
